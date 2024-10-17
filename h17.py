f39d6b7fb50459443106c75c405b78a31e308109
Its Tiwari <178887888+addhyan94@users.noreply.github.com>
2024-10-18 02:11:44 +0530

    Add files via upload

---
+++ h17.py
@@ -0,0 +1,4 @@
+# Flow control ke liye ....
+'''
+    Write a python program to find sum of digits of given number.
+'''
--- h3.1.py
+++ h3.1.py
@@ -1,3 +1,8 @@
+'''
+Write a python program to find area and perimeter of circle.
+area=3.14*r*r
+perimeter=2*3.14*r
+'''
 r=eval(input("Enter R= "))
 area=3.14*r*r
 perimeter=2*3.14*r
